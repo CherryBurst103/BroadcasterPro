d459746f195de8e6c464f9cc767206c76d9eee9d
CherryBurst103 <145829403+CherryBurst103@users.noreply.github.com>
2023-09-24 22:49:20 -0700

    Update BroadcastTask.php

--- src/Terpz710/BroadcasterPro/BroadcastTask.php
+++ src/Terpz710/BroadcasterPro/BroadcastTask.php
@@ -4,29 +4,30 @@ namespace Terpz710\BroadcasterPro;
 
 use pocketmine\plugin\Plugin;
 use pocketmine\scheduler\Task;
+use pocketmine\utils\Config;
 
 class BroadcastTask extends Task {
     private $plugin;
     private $messages;
     private $currentIndex;
 
-    public function __construct(Plugin $plugin) {
+    public function __construct(Plugin $plugin, array $messages) {
         $this->plugin = $plugin;
-        $this->messages = $plugin->getConfig()->get("message");
+        $this->messages = $messages;
         $this->currentIndex = 0;
     }
 
-    public function onRun(int $currentTick): void {
+    public function onRun() {
+        if ($this->currentIndex >= count($this->messages)) {
+            $this->currentIndex = 0;
+        }
+
         $message = $this->messages[$this->currentIndex];
-        
+
         foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
             $player->sendMessage($message);
         }
 
         $this->currentIndex++;
-
-        if ($this->currentIndex >= count($this->messages)) {
-            $this->currentIndex = 0;
-        }
     }
 }
