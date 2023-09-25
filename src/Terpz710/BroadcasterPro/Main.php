7ccf6ced505787bdf3dd26f033ae23c011a25f04
CherryBurst103 <145829403+CherryBurst103@users.noreply.github.com>
2023-09-24 22:28:57 -0700

    Add files via upload

---
+++ src/Terpz710/BroadcasterPro/Main.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Terpz710\BroadcasterPro;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\Task;
+
+class Main extends PluginBase {
+
+    public function onEnable(): void {
+        $broadcastInterval = $this->getConfig()->get("broadcast_interval", 1200);
+        $this->getScheduler()->scheduleRepeatingTask(new BroadcastTask($this), $broadcastInterval);
+    }
+}
+
+class BroadcastTask extends Task {
+    private $plugin;
+
+    public function __construct(Main $plugin) {
+        $this->plugin = $plugin;
+    }
+
+    public function onRun(int $currentTick) {
+        $message = $this->plugin->getConfig()->get("message");
+
+        foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
+            foreach ($message as $line) {
+                $player->sendMessage($line);
+            }
+        }
+    }
+}
