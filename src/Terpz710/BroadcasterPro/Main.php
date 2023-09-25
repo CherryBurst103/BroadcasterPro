dc8cb5b0c17c17e3d4dd84996448aa0bc50c2ae4
CherryBurst103 <145829403+CherryBurst103@users.noreply.github.com>
2023-09-24 22:36:29 -0700

    Update Main.php

--- src/Terpz710/BroadcasterPro/Main.php
+++ src/Terpz710/BroadcasterPro/Main.php
@@ -3,29 +3,11 @@
 namespace Terpz710\BroadcasterPro;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\scheduler\Task;
 
 class Main extends PluginBase {
 
     public function onEnable(): void {
         $broadcastInterval = $this->getConfig()->get("broadcast_interval", 1200);
-    }
-}
-
-class BroadcastTask extends Task {
-    private $plugin;
-
-    public function __construct(Main $plugin) {
-        $this->plugin = $plugin;
-    }
-
-    public function onRun(int $currentTick) {
-        $message = $this->plugin->getConfig()->get("message");
-
-        foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
-            foreach ($message as $line) {
-                $player->sendMessage($line);
-            }
-        }
+        $this->getScheduler()->scheduleRepeatingTask(new BroadcastTask($this), $broadcastInterval);
     }
 }
